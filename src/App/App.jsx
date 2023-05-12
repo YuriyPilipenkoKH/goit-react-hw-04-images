34465421539a645140d21d63f06dfd58b2db5e8b
Yuriy Pilipenko <yurik2061@gmail.com>
2023-05-12 09:48:10 +0300

    app hooked

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,137 +1,108 @@
-import React, { Component } from 'react'
-import { Container } from 'components/Container/Container'
-import Notiflix from 'notiflix'
-import Button from 'components/Button/Button'
-import { iconLoader } from 'utils/svgIcons'
-import { Searchbar } from 'components/Searchbar/Searchbar'
-import {fetchCard} from 'servises/fetch'
-import  ImageGallery  from 'components/ImageGallery/ImageGallery'
-import  Modal  from 'components/Modal/Modal'
-import Loader from 'components/Loader/Loader'
-import ToTopButton from 'components/ToTopButton/ToTopButton'
+import React, { useState, useEffect } from 'react';
+import { Container } from 'components/Container/Container';
+import Notiflix from 'notiflix';
+import Button from 'components/Button/Button';
+import { iconLoader } from 'utils/svgIcons';
+import { Searchbar } from 'components/Searchbar/Searchbar';
+import { fetchCard } from 'servises/fetch';
+import ImageGallery from 'components/ImageGallery/ImageGallery';
+import Modal from 'components/Modal/Modal';
+import Loader from 'components/Loader/Loader';
+import ToTopButton from 'components/ToTopButton/ToTopButton';
 
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [perPage] = useState(12);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImgUrl, setLargeImgUrl] = useState('');
+  const [error, setError] = useState(null);
+  // const [notification, setNotification] = useState(null);
+  const [showBtn, setShowBtn] = useState(false);
 
-export class App extends Component {
-  state = {
-    items : [],
-    page: 1,
-    perPage: 12,
-    query: '',
-    loading: false,
-    showModal: false,
-    showBtn: false,
-    largeImgUrl: '',
-    error: null,
-    notification: null,
-  }
-
-  handleSubmit =( query) => {
-
-    if(query === ''){
-      Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
-      this.setState({
-         items : [],
-         showBtn: false,
-        })
-     return 
+  const handleSubmit = (query) => {
+    if (query === '') {
+      Notiflix.Notify.failure(
+        'The search string cannot be empty. Please specify your search query.'
+      );
+      setItems([]);
+      return;
     }
 
-    this.setState({
-      query: query,
-      items : [],
-      page: 1,
-      loading: true,
-    })
+    setQuery(query);
+    setItems([]);
+    setPage(1);
+    setLoading(true);
+  };
 
-    const { perPage} = this.state
-    this.fetchGallery(query, 1, perPage)
-  }
+  const onLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+    setLoading(true);
+  };
 
-  onLoadMore = (e) => {
-   
-    this.setState(prev => {  
-      return {
-        page: prev.page +1,
-        loading: true,
-      } 
-    })
+  const onClickImg = (url) => {
+    setShowModal(true);
+    setLargeImgUrl(url);
+  };
 
-    const {page, perPage, query } = this.state
-   this.fetchGallery(query, page +1, perPage) 
-  }
+  const onModalClose = () => {
+    setShowModal(false);
+    setLargeImgUrl('');
+  };
 
-  onClickImg =(url) => {
-    this.setState({
-      showModal: true,
-      largeImgUrl: url,
-    })
-  } 
+  useEffect(() => {
+    if (query === '') return;
 
-  onModalClose =() => {
-    this.setState({
-      showModal: false,
-      largeImgUrl: '',
-    })
-  }
+    const fetchGallery = async () => {
+      try {
+        const response = await fetchCard(query, page, perPage);
+        setItems((prevItems) => [...prevItems, ...response]);
+        setLoading(false);
 
-  async fetchGallery(query, page, perPage) {
-    try {
-      const response = await fetchCard(query, page, perPage)
-      this.setState(prev => ({
-        items: [...prev.items, ...response],
-        loading: false,
-      }))
-       
-      if (response.length < 12) {
-        this.setState({showBtn: false})
+        if (response.length < 12) {
+          setShowBtn(false);
+        }
+        if (response.length === 12) {
+          setShowBtn(true);
+        }
+        if (response.length === 0) {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+      } catch (error) {
+        Notiflix.Notify.failure('Sorry, Page not found. Please try again.');
+        setError(error);
+        setLoading(false);
       }
-      if (response.length === 12) {
-        this.setState({showBtn: true})
-      }
-      if (response.length === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-      }
+    };
 
-    } catch (error) {
-      Notiflix.Notify.failure('Sorry, Page not found. Please try again.')
-      this.setState({ 
-        error,
-        loading: false,
-      })
-    }
-    finally{
-      this.setState({loadindg: false})
-    }
-  }
+    fetchGallery();
+  }, [query, page, perPage]);
 
+  return (
+    <Container>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery onClickImg={onClickImg} images={items} />
 
-  render() {
-    const {items, showBtn, showModal, largeImgUrl, loading} = this.state
-    return (
-      <Container>
-        <Searchbar onSubmit ={this.handleSubmit}></Searchbar>
-        <ImageGallery
-         onClickImg = {this.onClickImg}
-         images ={items}
-      
-        ></ImageGallery>
-        
-        {showBtn && (
-          <Button  onClick = {this.onLoadMore}>
+      {showBtn && (
+        <Button onClick={onLoadMore}>
           {iconLoader}
-          Load More </Button>
-        )}
-        
-        {showModal &&  <Modal  
-                onModalClose = {this.onModalClose}
-                picture = {largeImgUrl}
-                ></Modal>}
-        {loading && <Loader />}
-        {showBtn && <ToTopButton></ToTopButton>}
-        
-   
-      </Container>
-    )
-  }
-}
+          Load More
+        </Button>
+      )}
 
+      {showModal && (
+        <Modal onModalClose={onModalClose} picture={largeImgUrl} />
+      )}
+      {loading && <Loader />}
+      {showBtn && <ToTopButton />}
+
+      {error && <p>Error: {error.message}</p>}
+    </Container>
+  );
+};
+
+export default App;
